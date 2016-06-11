7f77cd63a12f970c1118342e1063872a374b9c77
Maik Merten <maikmerten@googlemail.com>
2016-06-11 12:23:21 +0200

    include a preliminary interrupt service routine in crt0.s (for now just directly return to normal control flow)

--- src/c/crt0.s
+++ src/c/crt0.s
@@ -1,8 +1,21 @@
+.macro rti
+custom0 0,0,0,0
+.endm
+
 .section .text
  
 .global _start
 _start:
+# reset vector at 0x0
+.=0x0
+j _init
 
+# interrupt vector at 0x8
+.=0x8
+# for now, just return from interrupt
+rti
+
+_init:
 # set up stack pointer
 li sp,4096
 
