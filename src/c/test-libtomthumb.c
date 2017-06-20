a4a26f8811b02e7f8816ce4cf6221a87c006d20c
Maik Merten <maikmerten@googlemail.com>
2017-06-20 20:50:50 +0200

    update test-libtomthumb to be slightly more impressive

--- src/c/test-libtomthumb.c
+++ src/c/test-libtomthumb.c
@@ -9,11 +9,31 @@ void main() {
 
 	while(1) {
 		set_printf_color(color);
-		printf("Hi there. This is a name: '%s'\n\r", "Hans");
-		printf("Number: %d   another one: %d\n\r", -1337, i);
-		printf("This printf has no argument\n\r");
-		printf("Another number: %d\n\r", -i);
+
+		printf("Serious greetz to %s!\n\r", "CCC Cologne");
+		printf("This runs on a RISC-V CPU on a FPGA!\n\r");
+		printf("This was greetz no. %d!\n\r\n\r", i);
+
+		for(int i = 0; i < 40; ++i) {
+			printf("#");
+			wasteTime(40000);
+		}
+
+
+		printf("\n\r\n\r");
+
 		i++;
 		color++;
 	}
 }
+
+
+
+void wasteTime(int iterations) {
+	for(int i = 0; i < iterations; ++i) {
+		volatile char* addr = (char*) (0x30000000);
+		char val = *addr;
+		*addr = val;
+	}
+}
+
